9ab2f0bd60f49182ffa9a86bb97af42a342dab7a
sainikhil849 <151641946+sainikhil849@users.noreply.github.com>
2024-08-12 23:32:55 +0530

    Create MovieListPage.js

---
+++ src/pages/MovieListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieList from '../components/MovieList';
+
+function MovieListPage() {
+  return (
+    <div className="content">
+      <h1>Movie List</h1>
+      <MovieList />
+    </div>
+  );
+}
+
+export default MovieListPage;
