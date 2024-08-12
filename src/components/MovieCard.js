eba32ed284a234830bb4dd0dcc64a76d72188180
sainikhil849 <151641946+sainikhil849@users.noreply.github.com>
2024-08-12 23:26:07 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,1 +1,26 @@
+import React from 'react';
+import { IMG_PATH } from '../global';
+import { useHistory } from 'react-router-dom';
+
+const MovieCard = ({ movie }) => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/detail?id=${movie.id}`);
+  };
+
+  return (
+    <div className="card" onClick={handleClick}>
+      <img className="card--image" src={`${IMG_PATH}${movie.poster_path}`} alt={movie.title} />
+      <div className="card--content">
+        <h3 className="card--title">{movie.title}</h3>
+        <p><small>Release Date: {movie.release_date}</small></p>
+        <p><small>Rating: {movie.vote_average}</small></p>
+        <p className="card--desc">{movie.overview}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
 
